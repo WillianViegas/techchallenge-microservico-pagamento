ad0fc188c8eed455d028432b964da91132de32d1
Willian Viegas <willianwed98@gmail.com>
2024-05-06 21:10:31 -0300

    Creating Controller and Repository tests

--- src/techchallenge-microservico-pagamento/techchallenge-ms-pagamento-test/Controllers/PagamentoControllerTests.cs
+++ src/techchallenge-microservico-pagamento/techchallenge-ms-pagamento-test/Controllers/PagamentoControllerTests.cs
@@ -1,32 +1,39 @@
-using Domain.Entities;
+using Microsoft.Extensions.Logging;
 using Moq;
+using techchallenge_microservico_pagamento.Controllers;
 using techchallenge_microservico_pagamento.Enums;
 using techchallenge_microservico_pagamento.Models;
-using techchallenge_microservico_pagamento.Services;
 using techchallenge_microservico_pagamento.Services.Interfaces;
 
-namespace techchallenge_ms_pagamento_test
+namespace techchallenge_ms_pagamento_test.Controllers
 {
-    public class PagamentoTests
+    public class PagamentoControllerTests
     {
         [Fact]
         public async Task FinalizarPedido()
         {
             //arrange
             var pedido = GetPedidoObj();
-
             var pedidoService = new Mock<IPedidoService>().Object;
+            var idPedido = Guid.NewGuid().ToString();
+            pedido.Id = idPedido;
 
             Mock.Get(pedidoService)
-                .Setup(service => service.FinalizarPedido(It.IsAny<string>()))
+                .Setup(service => service.FinalizarPedido(idPedido))
             .ReturnsAsync(pedido);
 
+            var mock = new Mock<ILogger<PagamentoController>>();
+            ILogger<PagamentoController> logger = mock.Object;
+
+            var controller = new PagamentoController(logger, pedidoService);
+
             //act
-            var result = await pedidoService.FinalizarPedido("difogidgjdg");
+            var result = await controller.FinalizarPedido(idPedido);
 
             //assert
             Assert.NotNull(result);
         }
+
 
         private Pedido GetPedidoObj()
         {
