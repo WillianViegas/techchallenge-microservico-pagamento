86236658d109b9443806c04d9a9fa23a31530284
Willian Viegas <willianwed98@gmail.com>
2024-06-23 21:14:20 -0300

    Creating SQSListenerService

--- src/techchallenge-microservico-pagamento/Application/Services/PedidoService.cs
+++ src/techchallenge-microservico-pagamento/Application/Services/PedidoService.cs
@@ -2,8 +2,15 @@
 using techchallenge_microservico_pagamento.Models;
 using techchallenge_microservico_pagamento.Repositories.Interfaces;
 using techchallenge_microservico_pagamento.Services.Interfaces;
-using techchallenge_microservico_pagamento.Repositories.Interfaces;
 using Microsoft.Extensions.Logging;
+using Amazon.S3;
+using Amazon.SQS;
+using Microsoft.Extensions.Configuration;
+using Amazon.SQS.Model;
+using System.Net;
+using Infra.SQS;
+using Amazon.SQS.ExtendedClient;
+using techchallenge_microservico_pedido.Models;
 
 namespace techchallenge_microservico_pagamento.Services
 {
@@ -13,13 +20,42 @@ namespace techchallenge_microservico_pagamento.Services
         private readonly ILogger<PedidoService> _logger;
         private readonly IPedidoRepository _pedidoRepository;
         private readonly ICarrinhoRepository _carrinhoRepository;
+        private readonly bool _useLocalStack;
+        private AmazonSQSClient _sqs;
+        private readonly string _queueUrlRead;
+        private readonly string _queueUrlSend;
+        private readonly bool _criarFila;
+        private readonly bool _enviarMensagem;
+        private readonly string _queueName;
+        private readonly string _bucketName;
+        private IAmazonSQS _amazonSQS;
+        private IAmazonS3 _amazonS3;
+        private readonly ISQSConfiguration _sQSConfiguration;
 
-
-        public PedidoService(ILogger<PedidoService> logger, IPedidoRepository pedidoRepository, ICarrinhoRepository carrinhoRepository)
+        public PedidoService(ILogger<PedidoService> logger, IPedidoRepository pedidoRepository, ICarrinhoRepository carrinhoRepository, IConfiguration config, IAmazonS3 s3, IAmazonSQS sqs, ISQSConfiguration sqsConfiguration)
         {
+            var criarFila = config.GetSection("SQSConfig").GetSection("CreateTestQueue").Value;
+            var enviarMensagem = config.GetSection("SQSConfig").GetSection("SendTestMessage").Value;
+            var useLocalStack = config.GetSection("SQSConfig").GetSection("useLocalStack").Value;
+
+            _sQSConfiguration = sqsConfiguration;
             _logger = logger;
             _pedidoRepository = pedidoRepository;
             _carrinhoRepository = carrinhoRepository;
+            _useLocalStack = Convert.ToBoolean(useLocalStack);
+            _criarFila = Convert.ToBoolean(criarFila);
+            _queueUrlRead = config.GetSection("QueueUrlRead").Value;
+            _queueUrlSend = config.GetSection("QueueUrlSend").Value;
+            _enviarMensagem = Convert.ToBoolean(enviarMensagem);
+            _queueName = config.GetSection("SQSConfig").GetSection("TestQueueName").Value;
+            _bucketName = config.GetSection("SQSExtendedClient").GetSection("S3Bucket").Value;
+            _amazonS3 = s3;
+            _amazonSQS = sqs;
+            _sqs = new AmazonSQSClient(Amazon.RegionEndpoint.GetBySystemName(
+                    string.IsNullOrEmpty(Environment.GetEnvironmentVariable("MY_SECRET")) || Environment.GetEnvironmentVariable("MY_SECRET").Equals("{MY_SECRET}")
+                    ? "us-east-1"
+                    : Environment.GetEnvironmentVariable("MY_SECRET")));
+
         }
 
 
@@ -37,7 +73,8 @@ namespace techchallenge_microservico_pagamento.Services
                     DataCriacao = DateTime.Now,
                     Numero = numeroPedido.Count + 1,
                     Usuario = pedido.Usuario,
-                    IdCarrinho = pedido.IdCarrinho
+                    IdCarrinho = pedido.IdCarrinho,
+                    IdPedidoOrigem = pedido.Id
                 };
 
                 await _pedidoRepository.CreatePedido(novoPedido);
@@ -55,7 +92,7 @@ namespace techchallenge_microservico_pagamento.Services
         {
             try
             {
-                var pedido = await GetPedidoById(id);
+                var pedido = await GetPedidoByIdOrigem(id);
                 if (pedido is null)
                 {
                     _logger.LogError("Pedido não encontrado");
@@ -83,6 +120,9 @@ namespace techchallenge_microservico_pagamento.Services
                     await _carrinhoRepository.UpdateCarrinho(carrinho.Id, carrinho);
                 }
 
+                var messageJson = Newtonsoft.Json.JsonConvert.SerializeObject(pedido);
+                await EnviarMessageSQS(messageJson);
+
                 return pedido;
             }
             catch (Exception ex)
@@ -92,11 +132,11 @@ namespace techchallenge_microservico_pagamento.Services
             }
         }
 
-        public async Task<Pedido> GetPedidoById(string id)
+        public async Task<Pedido> GetPedidoByIdOrigem(string id)
         {
             try
             {
-                return await _pedidoRepository.GetPedidoById(id);
+                return await _pedidoRepository.GetPedidoByIdOrigem(id);
             }
             catch (Exception ex)
             {
@@ -105,5 +145,70 @@ namespace techchallenge_microservico_pagamento.Services
             }
         }
 
+        async Task EnviarMessageSQS(string messageJson)
+        {
+            if (!_useLocalStack)
+            {
+                _sqs = await _sQSConfiguration.ConfigurarSQS();
+
+                await _sQSConfiguration.EnviarParaSQS(messageJson, _sqs, _queueUrlSend);
+            }
+            else
+            {
+                var configSQS = new AmazonSQSExtendedClient(_amazonSQS, new ExtendedClientConfiguration().WithLargePayloadSupportEnabled(_amazonS3, _bucketName));
+
+                if (_criarFila)
+                    await _sQSConfiguration.CreateMessageInQueueWithStatusASyncLocalStack(configSQS, _queueName);
+
+                if (_enviarMensagem)
+                    await _sQSConfiguration.SendTestMessageAsyncLocalStack(_queueUrlSend, configSQS);
+
+                await configSQS.SendMessageAsync(_queueUrlSend, messageJson);
+            }
+        }
+
+        public async Task RegistrarPedidos()
+        {
+            try
+            {
+                _logger.LogDebug("Reading queue...");
+
+                var response = await _amazonSQS.ReceiveMessageAsync(new ReceiveMessageRequest
+                {
+                    QueueUrl = _queueUrlRead,
+                    WaitTimeSeconds = 10,
+                    AttributeNames = new List<string> { "ApproximateReceiveCount" },
+                    MessageAttributeNames = new List<string> { "All" },
+                    MaxNumberOfMessages = 10
+                });
+
+                if (response.HttpStatusCode != HttpStatusCode.OK)
+                {
+                    _logger.LogError($"Error reading the queue: {_queueUrlRead}!");
+                    throw new AmazonSQSException($"Failed to GetMessages for queue {_queueUrlRead}. Response: {response.HttpStatusCode}");
+                }
+
+                foreach (var message in response.Messages)
+                {
+
+                    var obj = _sQSConfiguration.TratarMessage(message.Body);
+
+                    if (obj == null)
+                        continue;
+
+                    //chamar confirmar pedido
+                    var pedido = await CreatePedido(obj);
+                    Console.WriteLine(message.Body);
+                    _logger.LogInformation(message.Body);
+
+                    await _amazonSQS.DeleteMessageAsync(_queueUrlRead, message.ReceiptHandle);
+                    _logger.LogInformation($"Message deleted");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, $"Error processing messages for queue {_queueUrlRead}!");
+            }
+        }
     }
 }
---
+++ src/techchallenge-microservico-pagamento/Infra/SQS/ISQSConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.SQS;
+using Amazon.SQS.ExtendedClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using techchallenge_microservico_pagamento.Models;
+
+namespace Infra.SQS
+{
+    public interface ISQSConfiguration
+    {
+        public Task<AmazonSQSClient> ConfigurarSQS();
+        public Task EnviarParaSQS(string jsonMessage, AmazonSQSClient sqsclient, string queueUrl);
+        public Pedido? TratarMessage(string body);
+        public Task CreateMessageInQueueWithStatusASyncLocalStack(AmazonSQSExtendedClient sqs, string queueName);
+        public Task SendTestMessageAsyncLocalStack(string queue, AmazonSQSExtendedClient sqs);
+    }
+}
